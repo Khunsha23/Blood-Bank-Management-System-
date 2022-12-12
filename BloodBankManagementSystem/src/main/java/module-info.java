f45815af88596c5d70a191e23384fc6497d5805c
faahad-333 <fa21-bcs-111@cuilahore.edu.pk>
2022-12-12 20:29:47 +0500

    ok

--- BloodBankManagementSystem/src/main/java/module-info.java
+++ BloodBankManagementSystem/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module bbms.bloodbankmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
 
 
     opens bbms.bloodbankmanagementsystem to javafx.fxml;
