02f0e15a0ec224cba616d581694ea1f8410e3f4f
EmaanJawad <fa21-bcs-024@cuilahore.edu.pk>
2022-12-25 15:34:35 +0500

    emaan

--- BloodBankManagementSystem/src/main/java/bbms/bloodbankmanagementsystem/logindonorcontrol.java
+++ BloodBankManagementSystem/src/main/java/bbms/bloodbankmanagementsystem/logindonorcontrol.java
@@ -1,15 +1,19 @@
 package bbms.bloodbankmanagementsystem;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class logindonorcontrol {
+public class logindonorcontrol implements Validation {
 
         private Parent text;
         private Stage stage;
@@ -37,5 +41,46 @@ public class logindonorcontrol {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    private TextField MobileNumberTextField;
+    @FXML
+    private Label loginMsg;
+
+    @FXML
+    private PasswordField passwordField;
+
+    final String mobileNumber = "03436299271";
+    final String password = "dsaqw_1620";
+
+    @Override
+    public void loginValidation(ActionEvent event) throws IOException {
+
+        if(MobileNumberTextField.getText().isEmpty()&& passwordField.getText().isEmpty()){
+            loginMsg.setText("Please Enter Username and Password");
+        }
+        else if(MobileNumberTextField.getText().isEmpty()){
+            loginMsg.setText("Enter mobile number");
+        }else if (passwordField.getText().isEmpty()){
+            loginMsg.setText("Enter Password");
+        } else if (MobileNumberTextField.getText().equals(mobileNumber)&& passwordField.getText().equals(password)) {
+
+            loginMsg.setStyle("-fx-background-color:green;");
+            loginMsg.setText("Logged in Successfully");
+            root = FXMLLoader.load(getClass().getResource("Donorinfo.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }else{
+            loginMsg.setText("Invalid Credentials");
+        }
     }
+
+    @Override
+    public void SignupValidation(ActionEvent event) {
+
+    }
+
+
+}
 
